d24b6f53608b2254c12101ce0170472a1998f7c3
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2021-12-14 18:58:00 +0600

    ex234567

--- app/Http/Controllers/TuserController.php
+++ app/Http/Controllers/TuserController.php
@@ -64,7 +64,7 @@ class TuserController extends Controller
             $c = Company::where('companycode',$company)->first();
             \Storage::append('link.txt', $webhook);
             \Storage::append('link.txt', $company);
-            echo response()->json($c->toArray());
+           return 'asdfasdf';
         }
     }
     public function webhook(Request $request)
