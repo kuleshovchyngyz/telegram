142ec1222dfa5ad64bbc0cd12f7f01105f45acf8
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2021-12-16 16:50:46 +0600

    ex2345678

--- app/support/CreateTelegramUser.php
+++ app/support/CreateTelegramUser.php
@@ -177,7 +177,7 @@ class CreateTelegramUser
     public function reply(){
         \Storage::append('updateId.txt', $this->updateId );
         $t = TelegramBot::where('update_id',$this->updateId)->first();
-        \Storage::append('updateId.txt', json_encode($t->toArray(),JSON_UNESCAPED_UNICODE) );
+        \Storage::append('tel.txt', json_encode($t,JSON_UNESCAPED_UNICODE) );
         if($t){
             if($this->replyText !=''){
                 SendTelegramJob::dispatch([
